1091b0212e906f10d502a8c6da25c79739a0c4bf
Irena Pashchenko <ipashche@yahoo.com>
2014-05-20 11:11:27 -0700

    added hello world app.js file

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-var express = require('express');
-var app = express();
-var routes = require('./routes');
+var http = require('http'); //add the http module
+var myServer = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type" : "text/html"});
+  response.write("<b>Hello</b> World");
+  response.end();
+}); //create a server
 
-app.set('view engine', 'ejs');
+myServer.listen(3000);
 
-app.locals.pagetitle = "Awesome Website";
-
-app.get('/', routes.index);
-app.get('/about', routes.about);
-
-app.get('*', function(req, res) {
-  res.send('Bad Route');
-});
-
-var server = app.listen(3000, function() {
-  console.log('Listening on port 3000');
-}); 
+console.log("Go to http://localhost:3000 on your browser");
